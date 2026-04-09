317e1fcaae3756a37b744338d59963e76e5c1f4c
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-09 15:58:47 +0200

    aggiunto array bici con oggetti nome e peso

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict'
+// creo array bici
+
+// prendo la prima bici come la più leggera
+
+// ciclo su tutte le bici
+  // se il peso della bici corrente è minore
+    // aggiorno la bici più leggera
+
+// stampo la bici più leggera
+
+const bici = [
+    {nome: 'Ripper', peso: 7},
+    {nome: 'Zarok', peso: 3},
+    {nome: 'Hamilton', peso: 4},
+    {nome: 'Palethorn', peso: 6},
+    {nome: 'Kiya', peso: 8},
+    {nome: 'Conte', peso: 5},
+    {nome: 'Fortesque', peso: 10}
+];
